222340268e36908b9a209ff774f9c2873781358f
melihabi <melih@uni-bremen.de>
2024-09-18 20:36:25 +0200

    fix 3.0.2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import DraftRoom from './components/DraftRoom';
+import { DraftRoom } from './components';
 
 function App() {
   return (
